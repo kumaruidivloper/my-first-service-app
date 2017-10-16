fc4d8c148f1af73032a480a056420c358ee52944
Kumaravel Shanmugam <kumaravel.shanmugam@volvo.com>
2017-10-16 13:44:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "./services/user.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent implements OnInit {
+  
+    //title = 'app works!';
+    users;
+  
+    constructor(private userService: UserService) {
+    }
+  
+    ngOnInit() {
+       this.userService.getUsers().subscribe(p=>this.users = p);
+    }
+ }
